5ec5ba5a240151255a9879353d9ba26bed23d806
Michael <simmichaeld@gmail.com>
2025-07-21 08:38:25 -0400

    added base preferences and colorscheme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,6 +37,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- User added keymaps
+vim.keymap.set('n', 'D', '<C-d>', { desc = 'Jump down half a page' })
+vim.keymap.set('n', 'U', '<C-u>', { desc = 'Jump up half a page' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
